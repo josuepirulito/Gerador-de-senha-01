53faea1cf0c0b5a7f137a3a38ec151f72c82fec3
josuearaujo <josuegaraujo561@gmail.com>
2023-03-17 22:12:01 -0300

    josuearaujo

---
+++ script.js
@@ -0,0 +1,32 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizepassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerpassword = document.querySelector("#container-password");
+let charset ="abcdefghijlmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ0123456789!@";
+let novaSenha = "";
+
+sizepassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    sizepassword.innerHTML = this.value;
+}
+
+function generatepassword(){
+    let pass = "";
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+
+        pass += charset.charAt(Math.floor(Math.random() * n ))
+        
+    }
+    containerpassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+}
+function copypassword(){
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha);
+}
+
